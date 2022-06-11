acade455c12f94dde70eef840f7a4241b52d1ff0
Bekhruz Nutfilloyev <yoshlikmedia@gmail.com>
2022-06-11 09:06:58 +0500

    packages structure fixed

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from aiogram import executor
 
-import filters
-import handlers
-import middlewares
+import middlewares, filters, handlers
 from loader import dp
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
