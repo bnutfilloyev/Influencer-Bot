750c724b7fa58c8f8fc7740429575b642d8c715b
Bekhruz Nutfilloyev <yoshlikmedia@gmail.com>
2022-06-13 14:09:43 +0500

    adding text

--- handlers/users/create_post.py
+++ handlers/users/create_post.py
@@ -1,30 +1,48 @@
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 
+from data.texts import texts
+from keyboards.inline.categories import create_post_categories
 from loader import dp
 from aiogram.types.callback_query import CallbackQuery
 
 from states.States import Form
 
 
+@dp.callback_query_handler(text_contains="create")
+async def create_influencer(call, state: FSMContext):
+    async with state.proxy() as data:
+        # For get location
+        # if users.find_one({"user_id": call.from_user.id}).get("location", False):
+        #     await call.message.edit_text(
+        #         texts[data["lang"]]["location_question"],
+        #         reply_markup=await create_post_categories(data["lang"]),
+        #     )
+
+        await call.message.edit_text(
+            texts[data["lang"]]["create_post"],
+            reply_markup=await create_post_categories(data["lang"]),
+        )
+
+
 @dp.callback_query_handler(text_contains='video')
 async def create_video_post(call: CallbackQuery, state: FSMContext):
     async with state.proxy() as data:
-        await call.message.answer()
+        await call.message.answer(texts[data['lang']]['video_post'])
         await Form.post.set()
 
 
 @dp.callback_query_handler(text_contains='photo')
 async def create_video_post(call: CallbackQuery, state: FSMContext):
     async with state.proxy() as data:
-        await call.message.answer()
+        await call.message.answer(texts[data['lang']]['photo_post'])
         await Form.post.set()
 
 
 @dp.callback_query_handler(text_contains='text')
 async def create_video_post(call: CallbackQuery, state: FSMContext):
     async with state.proxy() as data:
-        await call.message.answer()
+        await call.message.answer(texts[data['lang']]['text_post'])
         await Form.post.set()
 
 
@@ -32,4 +50,4 @@ async def create_video_post(call: CallbackQuery, state: FSMContext):
 async def create_post(msg: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['post'] = msg.text
-        # thanking message
+        await state.finish()
